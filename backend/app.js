ef169bc455806243f03e81f44492f769fd32b5c3
Americo Tomas <americotomaswork@gmail.com>
2020-11-11 16:39:01 +0000

    Fullstack: Using Tokens, reate a new place (POST)

--- backend/app.js
+++ backend/app.js
@@ -1,60 +1,66 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/users-routes");
-const HttpError = require("./models/http-error");
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-  next();
+    next();
 });
 
-app.use("/api/places", placesRoutes);
-app.use("/api/users", usersRoutes);
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
-  throw error;
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
 });
 
 app.use((error, req, res, next) => {
-  if (req.file) {
-    fs.unlink(req.file.path, (err) => {
-      console.log(err);
-    });
-  }
-  if (res.headerSent) {
-    return next(error);
-  }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+    if (req.file) {
+        fs.unlink(req.file.path, (err) => {
+            console.log(err);
+        });
+    }
+    if (res.headerSent) {
+        return next(error);
+    }
+
+    console.log(req.body, error.message);
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://americotomas:12345@mern.d57qb.mongodb.net/data?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-  )
-  .then(() => {
-    app.listen(5000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(
+        'mongodb+srv://americotomas:12345@mern.d57qb.mongodb.net/data?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        }
+    )
+    .then(() => {
+        app.listen(5000);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
--- backend/util/location.js
+++ backend/util/location.js
@@ -2,32 +2,32 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
+const API_KEY = 'AIzaSyC21fASxgWK2P3RUjNEzm_f5j2s3P-9ivY';
 
 async function getCoordsForAddress(address) {
-  // return {
-  //   lat: 40.7484474,
-  //   lng: -73.9871516
-  // };
-  const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-      address
-    )}&key=${API_KEY}`
-  );
-
-  const data = response.data;
-
-  if (!data || data.status === 'ZERO_RESULTS') {
-    const error = new HttpError(
-      'Could not find location for the specified address.',
-      422
+    // return {
+    //   lat: 40.7484474,
+    //   lng: -73.9871516
+    // };
+    const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+            address
+        )}&key=${API_KEY}`
     );
-    throw error;
-  }
 
-  const coordinates = data.results[0].geometry.location;
+    const data = response.data;
 
-  return coordinates;
+    if (!data || data.status === 'ZERO_RESULTS') {
+        const error = new HttpError(
+            'Could not find location for the specified address.',
+            422
+        );
+        throw error;
+    }
+
+    const coordinates = data.results[0].geometry.location;
+
+    return coordinates;
 }
 
 module.exports = getCoordsForAddress;
